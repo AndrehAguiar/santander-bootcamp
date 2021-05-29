5fdf5af4ba8148167e2ecd658c7eed413e9a618d
AndrehAguiar <andreaguiar.g@gmail.com>
2021-05-29 01:40:37 -0300

    Added home page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { HomePageComponent } from './home/home-page/home-page.component';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardTodayComponent } from './dashboard/dashboard-today/dashboard-today.component';
 
 const routes: Routes = [
+  { path: '', component: HomePageComponent },
   { path: 'dashboard', component: DashboardPageComponent },
   { path: 'today', component: DashboardTodayComponent}
 ];
